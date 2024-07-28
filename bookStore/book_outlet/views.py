d280d9db35fa0a3c818a0ea1ba22ce8d4e43c34c
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-07-28 17:33:35 +0530

    Http404 error page added using get_object_or_404

--- bookStore/book_outlet/views.py
+++ bookStore/book_outlet/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import Http404
 
+from django.shortcuts import get_object_or_404, render
+
 # Create your views here.
 
 from .models import Book
@@ -12,13 +14,13 @@ def index(request):
 
 def book_detail(request, id):
 
-    try:
+    # try:
 
-        book = Book.objects.get(pk = id)
+    #     book = Book.objects.get(pk = id)
 
-    except:
-        raise Http404()
-    
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk = id)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
