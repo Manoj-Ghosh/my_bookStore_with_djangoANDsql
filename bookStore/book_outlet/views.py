a8721f9fe7a8b63b959f1fb0f84b02039078c6f1
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-07-28 18:39:53 +0530

    slug field added for url

--- bookStore/book_outlet/views.py
+++ bookStore/book_outlet/views.py
@@ -12,7 +12,7 @@ def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", { "books" : books})
 
-def book_detail(request, id):
+def book_detail(request, slug):
 
     # try:
 
@@ -20,7 +20,7 @@ def book_detail(request, id):
 
     # except:
     #     raise Http404()
-    book = get_object_or_404(Book, pk = id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
